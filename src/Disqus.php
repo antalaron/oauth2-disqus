b18d9565bf7c31f3b48fc7a250e28a50fe658ffa
Antal Áron <antalaron@antalaron.hu>
2023-05-03 18:59:35 +0200

    Add return types

--- src/Disqus.php
+++ src/Disqus.php
@@ -13,6 +13,7 @@ namespace Antalaron\DisqusOAuth2;
 
 use Antalaron\DisqusOAuth2\Exception\DisqusIdentityProviderException;
 use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
 use League\OAuth2\Client\Token\AccessToken;
 use Psr\Http\Message\ResponseInterface;
 
@@ -23,30 +24,30 @@ class Disqus extends AbstractProvider
 {
     public $domain = 'https://disqus.com';
 
-    public function getBaseAuthorizationUrl()
+    public function getBaseAuthorizationUrl(): string
     {
         return $this->domain.'/api/oauth/2.0/authorize/';
     }
 
-    public function getBaseAccessTokenUrl(array $params)
+    public function getBaseAccessTokenUrl(array $params): string
     {
         return $this->domain.'/api/oauth/2.0/access_token/';
     }
 
-    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    public function getResourceOwnerDetailsUrl(AccessToken $token): string
     {
         return $this->domain.'/api/3.0/users/details.json?access_token='.$token
             .'&api_key='.$this->clientId.'&api_secret='.$this->clientSecret;
     }
 
-    protected function getDefaultScopes()
+    protected function getDefaultScopes(): array
     {
         return [
             'read',
         ];
     }
 
-    protected function checkResponse(ResponseInterface $response, $data)
+    protected function checkResponse(ResponseInterface $response, $data): void
     {
         if ($response->getStatusCode() >= 400) {
             throw DisqusIdentityProviderException::clientException($response, $data);
@@ -55,7 +56,7 @@ class Disqus extends AbstractProvider
         }
     }
 
-    protected function createResourceOwner(array $response, AccessToken $token)
+    protected function createResourceOwner(array $response, AccessToken $token): ResourceOwnerInterface
     {
         return new DisqusResourceOwner($response);
     }
